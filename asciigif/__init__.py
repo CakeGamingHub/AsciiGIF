64b2ea63e45f4be67b2b9ae84610606f6bd8dff4
Motherflippin <cakephone5@gmail.com>
2018-03-24 12:45:23 +0000

    Untested first commit

---
+++ asciigif/__init__.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+
+class AsciiGif:
+    def __init__(self, ListOfFrames, FrameRate):
+        self.Frames = ListOfFrames
+        self.FrameRate = FrameRate
+        self.ClearCommand = "os.system('cls')"
+
+    def SetClearCommand(self, Command):
+        self.ClearCommand = Command
+
+    def DrawLogic(self):
+        for i in self.Frames:
+            print(i)
+            exec(self.ClearCommand)
+            time.sleep(1 / self.FrameRate)
+
+    def Draw(self, Iterations):
+        CurrentIteration = 1
+        while CurrentIteration <= Iterations:
+            self.DrawLogic()
+            CurrentIteration += 1
+        while Iterations == -1:
+            self.DrawLogic()
+
